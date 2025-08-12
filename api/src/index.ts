73320dc1be0c572179d721b5d8cb23981e7fd565
Stains Rubus <stainsrubus@gmail.com>
2025-08-12 11:56:43 +0530

    minor comit

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import { app } from "./setup";
 config();
 
 const PORT = 4000;
-const HOST ="localhost"
+const HOST ="0.0.0.0"
 
 app.listen({ port: PORT, hostname: HOST }, () => {
   console.log(`Listening on http://${HOST}:${PORT}`);
